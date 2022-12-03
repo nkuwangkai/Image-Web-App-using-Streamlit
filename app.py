4416fa9392adb817367225d46f46df74110cd7ab
nkuwangkai <82133910+nkuwangkai@users.noreply.github.com>
2022-12-03 19:05:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import xgboost as xgb
-import sklearn
-from sklearn.utils import shuffle
 from bayes_opt import BayesianOptimization
 from xgboost.sklearn import XGBClassifier
 import joblib
