ce259d7f840d1f317023b80fa35208334885ffe7
nkuwangkai <82133910+nkuwangkai@users.noreply.github.com>
2023-02-18 19:03:25 +0800

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -10,7 +10,7 @@ features
 kmedoids = KMedoids(n_clusters=3,metric='euclidean',method='pam',random_state=123).fit(features)
 
 # Title
-st.header("Clustering for 2D-UCG")
+st.header("Clustering for 2D-UCG")	
 
 LA = st.number_input("LA(mm)",step=1)
 LV = st.number_input("LV(mm)",step=1)
@@ -20,12 +20,14 @@ IVS = st.number_input("IVS(mm)",step=1)
 LVPW = st.number_input("LVPW(mm)",step=1)
 AO = st.number_input("AO(mm)",step=1)
 RVTO = st.number_input("RVTO(mm)",step=1)
+MPA = st.number_input("AO(mm)",step=1)
+ESD = st.number_input("RVTO(mm)",step=1)
 
 # If button is pressed
 if st.button("Cluster"):
     
-    data_new = pd.DataFrame([[LA, LV, RA, RV, IVS, LVPW, AO, RVTO]],
-                     columns=["LA", "LV", "RA", "RV", "IVS", "LVPW", "AO", "RVTO"])
+    data_new = pd.DataFrame([[LA, LV, RA, RV, IVS, LVPW, AO, RVTO,MPA,ESD]],
+                     columns=["LA", "LV", "RA", "RV", "IVS", "LVPW", "AO", "RVTO", "MPA", "ESD"])
     # Store inputs into dataframe
     features_new = data_new.drop(columns=[])
 
