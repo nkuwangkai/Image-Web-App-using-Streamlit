6250ebaeb3c0eb98b0ea33fae0f80ab69ed5ffc0
nkuwangkai <82133910+nkuwangkai@users.noreply.github.com>
2023-03-12 23:29:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,55 +5,51 @@ from xgboost.sklearn import XGBClassifier
 import streamlit as st
 
 data3 = pd.read_csv("https://raw.githubusercontent.com/nkuwangkai/app-for-mortality-prediction/main/data3.csv",thousands=',')
-Xtrain = (data3.iloc[:,1:22]) 
+Xtrain = (data3.iloc[:,1:18]) 
 Ytrain = (data3.iloc[:,0])
 
 clf = XGBClassifier(objective='binary:logistic',
               booster='gbtree',
-              colsample_bytree=0.558759,
-              gamma=0.1477409,
-              learning_rate=0.08694605,
-              max_delta_step=8,
+              colsample_bytree=0.6447264,
+              gamma=0.1934144,
+              learning_rate=0.1527683,
+              max_delta_step=10,
               max_depth=3,
-              min_child_weight=37,
-              n_estimators=92,
-              subsample=0.6428299)
+              min_child_weight=10,
+              n_estimators=95,
+              subsample=0.6293208)
 
 clf.fit(Xtrain,Ytrain)
 
 # Title
-st.header("Machine learning app for in-hospital mortality prediction")
+st.header("Machine learning app for in-hospital mortality prediction")								
 
-Age = st.number_input("Age (years)",step=1,min_value=0)
-Temperature = st.number_input("Temperature (℃)",min_value=0.0)
-RespiratoryRate = st.number_input("RespiratoryRate (breaths per minute)",step=1,min_value=0)
-HeartRate = st.number_input("HeartRate (beats per minute)",step=1,min_value=0)
-SBP = st.number_input("SBP (mmHg)",step=1,min_value=0)
-AG = st.number_input("AG",step=1,min_value=0)
-BUN = st.number_input("BUN (mg/dL)",step=1,min_value=0)
-MCHC = st.number_input("MCHC (g/L)",min_value=0.0)
-MCV = st.number_input("MCV (fL)",step=1,min_value=0)
-RDW = st.number_input("RDW",min_value=0.0)
-WBC = st.number_input("WBC (×109/L)",min_value=0.0)
-Race = st.number_input("Race (white=1,black=2,others=3)",step=1,min_value=1,max_value=3)
-Norepinephrine = st.number_input("Norepinephrine (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-Dopamine = st.number_input("Dopamine (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-Phenylephrine = st.number_input("Phenylephrine (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-Vasopressin = st.number_input("Vasopressin (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-Vent = st.number_input("Vent (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-Intubated = st.number_input("Intubated (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-MC = st.number_input("MC (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-HepF = st.number_input("HepF (No=0,Yes=1)",step=1,min_value=0,max_value=1)
+age = st.number_input("Age (years)",step=1,min_value=0)
+diabetes = st.number_input("diabetes (No=0,Yes=1)",step=1,min_value=0,max_value=1)
+SevereLiverDisease = st.number_input("severe liver disease (No=0,Yes=1)",step=1,min_value=0,max_value=1)
+myocardial_infarct = st.number_input("myocardial infarction (No=0,Yes=1)",step=1,min_value=0,max_value=1)
+SOFA = st.number_input("SOFA (scores)",step=1,min_value=0)
+APSⅢ = st.number_input("APS Ⅲ (scores)",step=1,min_value=0)
+RespiratoryRate = st.number_input("respiratory rate (breaths per minute)",step=1,min_value=0)
+mchc = st.number_input("mchc (%)",step=0.1,min_value=0)
+rdw = st.number_input("rdw (%)",step=0.1,min_value=0)
+TotalBilirubin = st.number_input("total bilirubin (mg/dL)",step=0.1,min_value=0)
+dopamine = st.number_input("dopamine (No=0,Yes=1)",step=1,min_value=0,max_value=1)
+dobutamine = st.number_input("dobutamine (No=0,Yes=1)",step=1,min_value=0,max_value=1)
+phenylephrine = st.number_input("phenylephrine (No=0,Yes=1)",step=1,min_value=0,max_value=1)
+epinephrine = st.number_input("epinephrine (No=0,Yes=1)",step=1,min_value=0,max_value=1)
+vasopressin = st.number_input("vasopressin (No=0,Yes=1)",step=1,min_value=0,max_value=1)
+blocker = st.number_input("beta blocker (No=0,Yes=1)",step=1,min_value=0,max_value=1)
+ventilation = st.number_input("ventilation (No=0,Yes=1)",step=1,min_value=0,max_value=1)
 
 # If button is pressed
 if st.button("Predict"):
 
     # Store inputs into dataframe
-    X = pd.DataFrame([[Age, Temperature, RespiratoryRate, HeartRate, SBP, AG, BUN, MCHC, MCV, RDW, WBC, Race,
-                       Norepinephrine, Dopamine, Phenylephrine, Vasopressin, Vent, Intubated, MC, HepF]],
-                     columns=["Age", "Temperature", "RespiratoryRate", "HeartRate", "SBP", "AG", "BUN", "MCHC", "MCV",
-                              "RDW", "WBC", "Race", "Norepinephrine", "Dopamine", "Phenylephrine", "Vasopressin",
-                              "Vent", "Intubated", "MC", "HepF"])
+    X = pd.DataFrame([[age, diabetes, SevereLiverDisease, myocardial_infarct, SOFA, APSⅢ, RespiratoryRate, mchc, rdw, TotalBilirubin,
+                       dopamine, dobutamine, phenylephrine, epinephrine, vasopressin, blocker, ventilation]],
+                     columns=["age", "diabetes", "SevereLiverDisease", "myocardial_infarct", "SOFA", "APSⅢ", "RespiratoryRate", "mchc", "rdw", "TotalBilirubin",
+                              "dopamine", "dobutamine", "phenylephrine", "epinephrine", "vasopressin", "blocker", "ventilation"])
 
     # Get prediction
     prediction = clf.predict(X)[0]
@@ -61,4 +57,4 @@ if st.button("Predict"):
 
     # Output prediction
     st.text(f"in-hospital survive/mortality probability [{prectionProbability}]")
-    st.text(f"in-hospital mortality prediction [{prediction}]")
+    st.text(f"in-hospital mortality prediction [{prediction}] (0=survive, 1=mortality")
