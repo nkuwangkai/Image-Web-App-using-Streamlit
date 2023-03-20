da53ba72bf415a884410f8d8d495c94c2ef970af
nkuwangkai <82133910+nkuwangkai@users.noreply.github.com>
2023-03-20 23:23:32 +0800

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -7,7 +7,7 @@ import streamlit as st
 data = pd.read_csv("https://raw.githubusercontent.com/nkuwangkai/app-for-mortality-prediction/main/data2.csv",thousands=',')
 features = data.drop(columns=[])
 features
-kmedoids = KMedoids(n_clusters=3,metric='euclidean',method='pam',random_state=123).fit(features)
+kmedoids = KMedoids(n_clusters=2,metric='euclidean',method='pam',random_state=123).fit(features)
 
 # Title
 st.header("Clustering for 2D-UCG")	
@@ -20,14 +20,14 @@ IVS = st.number_input("IVS (mm)",step=1)
 LVPW = st.number_input("LVPW (mm)",step=1)
 AO = st.number_input("AO (mm)",step=1)
 RVTO = st.number_input("RVTO (mm)",step=1)
-MPA = st.number_input("MPA (mm)",step=1)
 ESD = st.number_input("ESD (mm)",step=1)
+MPA = st.number_input("MPA (mm)",step=1)
 
 # If button is pressed
 if st.button("Cluster"):
     
     data_new = pd.DataFrame([[LA, LV, RA, RV, IVS, LVPW, AO, RVTO,MPA,ESD]],
-                     columns=["LA", "LV", "RA", "RV", "IVS", "LVPW", "AO", "RVTO", "MPA", "ESD"])
+                     columns=["LA", "LV", "RA", "RV", "IVS", "LVPW", "AO", "RVTO", "ESD", "MPA"])
     # Store inputs into dataframe
     features_new = data_new.drop(columns=[])
 
