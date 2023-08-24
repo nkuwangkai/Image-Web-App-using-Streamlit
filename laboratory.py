777dedd5a559509a72d292932f015690963d0721
nkuwangkai <82133910+nkuwangkai@users.noreply.github.com>
2023-08-24 22:57:11 +0800

    Update laboratory.py

--- laboratory.py
+++ laboratory.py
@@ -1,8 +1,9 @@
+import streamlit as st
 import pandas as pd
 import numpy as np
 import xgboost
+from sklearn import datasets
 from xgboost.sklearn import XGBClassifier
-import streamlit as st
 
 data3 = pd.read_csv("https://raw.githubusercontent.com/nkuwangkai/app-for-mortality-prediction/main/laboratory.csv",thousands=',',encoding='GBK')
 Xtrain = (data3.iloc[:,1:19]) 
@@ -24,37 +25,39 @@ clf.fit(Xtrain,Ytrain)
 # Title
 st.header("Machine learning app for integration in laboratory test")								
 
-age = st.number_input("age (years)",step=1,min_value=0)
-diabetes = st.number_input("diabetes (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-SevereLiverDisease = st.number_input("severe liver disease (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-myocardial_infarct = st.number_input("myocardial infarction (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-SOFA = st.number_input("SOFA (scores)",step=1,min_value=0)
-APSⅢ = st.number_input("APS Ⅲ (scores)",step=1,min_value=0)
-RespiratoryRate = st.number_input("respiratory rate (breaths per minute)",step=1,min_value=0)
-mchc = st.number_input("MCHC (%)",min_value=0.0)
-rdw = st.number_input("RDW (%)",min_value=0.0)
-TotalBilirubin = st.number_input("total bilirubin (mg/dL)",min_value=0.0)
-dopamine = st.number_input("dopamine (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-dobutamine = st.number_input("dobutamine (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-phenylephrine = st.number_input("phenylephrine (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-epinephrine = st.number_input("epinephrine (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-vasopressin = st.number_input("vasopressin (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-blocker = st.number_input("beta blocker (No=0,Yes=1)",step=1,min_value=0,max_value=1)
-ventilation = st.number_input("ventilation (No=0,Yes=1)",step=1,min_value=0,max_value=1)
+# 用户输入数据
+def user_input_features():
+    wbc = st.sidebar.slider('wbc', 0.1, 100, 10)
+    hemoglobin = st.sidebar.slider('hemoglobin', 2.0, 30, 10)
+    rdw = st.sidebar.slider('rdw', 10, 40, 20)
+    albumin = st.sidebar.slider('albumin', 0.1, 6.0, 3.0)
+    total_bilirubin = st.sidebar.slider('total bilirubin', 0.1, 50, 10)
+    bun = st.sidebar.slider('bun',1.0, 250, 10)
+    creatinine = st.sidebar.slider('creatinine', 0.1, 25, 5)
+    glucose = st.sidebar.slider('glucose', 15, 500, 50)
+    sodium = st.sidebar.slider('sodium', 80, 180, 150)
+    chloride = st.sidebar.slider('chloride', 60, 150, 100)
+    neutrophils_lymphocytes_ratio = st.sidebar.slider('neutrophils - lymphocytes ratio', 0.1, 10000, 100)
+    hematocrit_rdw_ratio = st.sidebar.slider('hematocrit - rdw ratio', 0.1, 5.0, 3.0)
+    rdw_albumin_ratio = st.sidebar.slider('rdw - albumin ratio', 2.0, 42, 5.0)
+    neutrophils_albumin_ratio = st.sidebar.slider('neutrophils - albumin ratio', 0.1, 60, 4.0)
+    bun_creatinine_ratio = st.sidebar.slider('bun - creatinine ratio', 1.0, 190, 15)
+    albumin_total_bilirubin_ratio = st.sidebar.slider('albumin - total bilirubin ratio', 1.0, 45, 15)
+    alt_ast_ratio = st.sidebar.slider('alt - ast ratio', 0.1, 18, 2.0)
+    bun_albumin_ratio = st.sidebar.slider('bun - albumin ratio', 0.1, 90, 20)
+    data = {'wbc': wbc, 'hemoglobin': hemoglobin,
+            'rdw': rdw,'albumin': albumin,
+            'total_bilirubin': total_bilirubin,'bun': bun,
+            'creatinine': creatinine,'glucose': glucose,
+            'sodium': sodium,'chloride': chloride,
+            'neutrophils_lymphocytes_ratio': neutrophils_lymphocytes_ratio,'hematocrit_rdw_ratio': hematocrit_rdw_ratio,
+            'rdw_albumin_ratio': rdw_albumin_ratio,'neutrophils_albumin_ratio': neutrophils_albumin_ratio,
+            'bun_creatinine_ratio': bun_creatinine_ratio,'albumin_total_bilirubin_ratio': albumin_total_bilirubin_ratio,
+            'alt_ast_ratio': alt_ast_ratio,'bun_albumin_ratio': bun_albumin_ratio}
+    features = pd.DataFrame(data, index=[0])
+    return features
+df = user_input_features()
 
-# If button is pressed
-if st.button("Predict"):
-
-    # Store inputs into dataframe
-    X = pd.DataFrame([[age, diabetes, SevereLiverDisease, myocardial_infarct, SOFA, APSⅢ, RespiratoryRate, mchc, rdw, TotalBilirubin,
-                       dopamine, dobutamine, phenylephrine, epinephrine, vasopressin, blocker, ventilation]],
-                     columns=["age", "diabetes", "SevereLiverDisease", "myocardial_infarct", "SOFA", "APSⅢ", "RespiratoryRate", "mchc", "rdw", "TotalBilirubin",
-                              "dopamine", "dobutamine", "phenylephrine", "epinephrine", "vasopressin", "blocker", "ventilation"])
-
-    # Get prediction
-    prediction = clf.predict(X)[0]
-    prectionProbability = clf.predict_proba(X)
-
-    # Output prediction
-    st.text(f"in-hospital survive/mortality probability {prectionProbability}")
-    st.text(f"in-hospital mortality prediction [{prediction}] (0=survive, 1=mortality)")
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+st.text(f"In-hospital mortality risk score is {prediction_proba}")
